3a791137a7eca09010008d169e2e236bba204cc9
hadesh <hades_h@126.com>
2014-10-21 16:46:07 +0800

    add tableview
    add tableview,
    add place search

--- HelloAmap/HelloAmap/ViewController.m
+++ HelloAmap/HelloAmap/ViewController.m
@@ -12,13 +12,21 @@
 
 #define APIKey      @"95ed2da3e9f4ece6319afbc437fc0b01"
 
-@interface ViewController ()<MAMapViewDelegate, AMapSearchDelegate>
+#define kDefaultLocationZoomLevel       16.1
+#define kDefaultControlMargin           22
+
+@interface ViewController ()<MAMapViewDelegate, AMapSearchDelegate, UITableViewDataSource, UITableViewDelegate>
 {
     MAMapView *_mapView;
     AMapSearchAPI *_search;
     
     CLLocation *_currentLocation;
     UIButton *_locationButton;
+    
+    UITableView *_tableView;
+    NSArray *_pois;
+    
+    NSMutableArray *_annotations;
 }
 @end
 
@@ -30,16 +38,20 @@
     [self initMapView];
     [self initSearch];
     [self initControls];
+    
+    [self initTableView];
+    
+    [self initAttributes];
 }
 
 - (void)initMapView
 {
     [MAMapServices sharedServices].apiKey = APIKey;
-    _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds))];
+    _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds) * 0.5)];
     
     _mapView.delegate = self;
-    _mapView.compassOrigin = CGPointMake(_mapView.compassOrigin.x, 22);
-    _mapView.scaleOrigin = CGPointMake(_mapView.scaleOrigin.x, 22);
+    _mapView.compassOrigin = CGPointMake(_mapView.compassOrigin.x, kDefaultControlMargin);
+    _mapView.scaleOrigin = CGPointMake(_mapView.scaleOrigin.x, kDefaultControlMargin);
     
     [self.view addSubview:_mapView];
     
@@ -54,17 +66,44 @@
 - (void)initControls
 {
     _locationButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    _locationButton.frame = CGRectMake(20, CGRectGetHeight(_mapView.bounds) - 80, 40, 40);
+    _locationButton.frame = CGRectMake(kDefaultControlMargin, CGRectGetHeight(_mapView.bounds) - 80, 40, 40);
     _locationButton.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
     _locationButton.backgroundColor = [UIColor whiteColor];
-    _locationButton.layer.cornerRadius = 5;
     
     [_locationButton addTarget:self action:@selector(locateAction) forControlEvents:UIControlEventTouchUpInside];
     
     [_locationButton setImage:[UIImage imageNamed:@"location_no"] forState:UIControlStateNormal];
     
     [_mapView addSubview:_locationButton];
+    
+    //
+    UIButton *searchButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    searchButton.frame = CGRectMake(80, CGRectGetHeight(_mapView.bounds) - 80, 40, 40);
+    searchButton.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
+    searchButton.backgroundColor = [UIColor whiteColor];
+    [searchButton setImage:[UIImage imageNamed:@"search"] forState:UIControlStateNormal];
+    
+    [searchButton addTarget:self action:@selector(searchAction) forControlEvents:UIControlEventTouchUpInside];
+    
+    [_mapView addSubview:searchButton];
 
+}
+
+- (void)initAttributes
+{
+    _annotations = [NSMutableArray array];
+    _pois = nil;
+}
+
+- (void)initTableView
+{
+    CGFloat halfHeight = CGRectGetHeight(self.view.bounds) * 0.5;
+    
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, halfHeight, CGRectGetWidth(self.view.bounds), halfHeight) style:UITableViewStylePlain];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    
+    [self.view addSubview:_tableView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -74,11 +113,29 @@
 
 #pragma mark - Helpers
 
+- (void)searchAction
+{
+    if (_currentLocation == nil || _search == nil)
+    {
+        NSLog(@"search failed");
+        return;
+    }
+    
+    AMapPlaceSearchRequest *request = [[AMapPlaceSearchRequest alloc] init];
+    request.searchType = AMapSearchType_PlaceAround;
+    request.location = [AMapGeoPoint locationWithLatitude:_currentLocation.coordinate.latitude longitude:_currentLocation.coordinate.longitude];
+    
+    request.keywords = @"餐饮";
+    
+    [_search AMapPlaceSearch:request];
+}
+
 - (void)locateAction
 {
     if (_mapView.userTrackingMode != MAUserTrackingModeFollow)
     {
-        [_mapView setUserTrackingMode:MAUserTrackingModeFollow animated:YES];
+        _mapView.userTrackingMode = MAUserTrackingModeFollow;
+        [_mapView setZoomLevel:kDefaultLocationZoomLevel animated:YES];
     }
 }
 
@@ -113,7 +170,23 @@
     
     _mapView.userLocation.title = title;
     _mapView.userLocation.subtitle = response.regeocode.formattedAddress;
+}
+
+- (void)onPlaceSearchDone:(AMapPlaceSearchRequest *)request response:(AMapPlaceSearchResponse *)response
+{
+    NSLog(@"request: %@", request);
+    NSLog(@"response: %@", response);
     
+    if (response.pois.count > 0)
+    {
+        _pois = response.pois;
+        
+        [_tableView reloadData];
+        
+        // 清空标注
+        [_mapView removeAnnotations:_annotations];
+        [_annotations removeAllObjects];
+    }
 }
 
 #pragma mark - MAMapViewDelegate
@@ -146,5 +219,49 @@
     }
 }
 
+#pragma mark - UITableViewDataSource
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"cellIdentifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+    }
+    
+    AMapPOI *poi = _pois[indexPath.row];
+    
+    cell.textLabel.text = poi.name;
+    cell.detailTextLabel.text = poi.address;
+    
+    return cell;
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _pois.count;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    AMapPOI *poi = _pois[indexPath.row];
+    
+    MAPointAnnotation *annotation = [[MAPointAnnotation alloc] init];
+    annotation.coordinate = CLLocationCoordinate2DMake(poi.location.latitude, poi.location.longitude);
+    annotation.title = poi.name;
+    annotation.subtitle = poi.address;
+    
+    [_mapView addAnnotation:annotation];
+    
+    [_annotations addObject:annotation];
+}
 
 @end
