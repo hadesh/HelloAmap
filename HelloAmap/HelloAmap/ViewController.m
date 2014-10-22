0b83ec8752c981ad837eb6b1ce28624673e072dc
hadesh <hades_h@126.com>
2014-10-22 17:19:47 +0800

    change to no map location
    location without map

--- HelloAmap/HelloAmap/ViewController.m
+++ HelloAmap/HelloAmap/ViewController.m
@@ -13,26 +13,18 @@
 
 #define APIKey      @"95ed2da3e9f4ece6319afbc437fc0b01"
 
-#define kDefaultLocationZoomLevel       16.1
-#define kDefaultControlMargin           22
-#define kDefaultCalloutViewMargin       -8
-
-@interface ViewController ()<MAMapViewDelegate, AMapSearchDelegate, UITableViewDataSource, UITableViewDelegate, UIGestureRecognizerDelegate>
+@interface ViewController ()<MAMapViewDelegate, AMapSearchDelegate, UITableViewDataSource, UITableViewDelegate>
 {
     MAMapView *_mapView;
     AMapSearchAPI *_search;
     
     CLLocation *_currentLocation;
-    UIButton *_locationButton;
     
     UITableView *_tableView;
     NSArray *_pois;
-    NSMutableArray *_annotations;
     
-    UILongPressGestureRecognizer *_longPressGesture;
-    MAPointAnnotation *_destinationPoint;
-    
-    NSArray *_pathPolylines;
+    UILabel *_titleLabel;
+    UILabel *_subtitleLabel;
 }
 @end
 
@@ -43,21 +35,19 @@
     // Do any additional setup after loading the view, typically from a nib.
     [self initMapView];
     [self initSearch];
-    [self initControls];
     [self initTableView];
+    [self initControls];
     [self initAttributes];
 }
 
 - (void)initMapView
 {
     [MAMapServices sharedServices].apiKey = APIKey;
-    _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds) * 0.5)];
+    _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, 1, 1)];
     
     _mapView.delegate = self;
-    _mapView.compassOrigin = CGPointMake(_mapView.compassOrigin.x, kDefaultControlMargin);
-    _mapView.scaleOrigin = CGPointMake(_mapView.scaleOrigin.x, kDefaultControlMargin);
     
-    [self.view addSubview:_mapView];
+//    [self.view addSubview:_mapView];
     
     _mapView.showsUserLocation = YES;
 }
@@ -69,56 +59,47 @@
 
 - (void)initControls
 {
-    _locationButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    _locationButton.frame = CGRectMake(kDefaultControlMargin, CGRectGetHeight(_mapView.bounds) - 60, 40, 40);
-    _locationButton.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
-    _locationButton.backgroundColor = [UIColor whiteColor];
-    
-    [_locationButton addTarget:self action:@selector(locateAction) forControlEvents:UIControlEventTouchUpInside];
-    
-    [_locationButton setImage:[UIImage imageNamed:@"location_no"] forState:UIControlStateNormal];
-    
-    [_mapView addSubview:_locationButton];
-    
     //
     UIButton *searchButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    searchButton.frame = CGRectMake(80, CGRectGetHeight(_mapView.bounds) - 60, 40, 40);
+    searchButton.frame = CGRectMake(22, CGRectGetHeight(self.view.bounds) - 80, 40, 40);
     searchButton.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
-    searchButton.backgroundColor = [UIColor whiteColor];
+    searchButton.backgroundColor = [UIColor blackColor];
     [searchButton setImage:[UIImage imageNamed:@"search"] forState:UIControlStateNormal];
     
     [searchButton addTarget:self action:@selector(searchAction) forControlEvents:UIControlEventTouchUpInside];
     
-    [_mapView addSubview:searchButton];
+    [self.view addSubview:searchButton];
     
-    //
-    UIButton *pathButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    pathButton.frame = CGRectMake(140, CGRectGetHeight(_mapView.bounds) - 60, 40, 40);
-    pathButton.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin;
-    pathButton.backgroundColor = [UIColor whiteColor];
-    [pathButton setImage:[UIImage imageNamed:@"path"] forState:UIControlStateNormal];
     
-    [pathButton addTarget:self action:@selector(pathAction) forControlEvents:UIControlEventTouchUpInside];
+    UIView *bgView = [[UIView alloc] initWithFrame:CGRectMake(80, CGRectGetHeight(self.view.bounds) - 80, 220, 40)];
+    bgView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.6];
     
-    [_mapView addSubview:pathButton];
-
+    _titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(2, 2, 200, 20)];
+    _titleLabel.textColor = [UIColor whiteColor];
+    _titleLabel.font = [UIFont boldSystemFontOfSize:14];
+    _titleLabel.text = @"xxx";
+    
+    [bgView addSubview:_titleLabel];
+    
+    _subtitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(2, 22, 200, 18)];
+    _subtitleLabel.textColor = [UIColor lightGrayColor];
+    _subtitleLabel.font = [UIFont boldSystemFontOfSize:12];
+    _subtitleLabel.text = @"xxxxxxxx";
+    
+    [bgView addSubview:_subtitleLabel];
+    
+    [self.view addSubview:bgView];
 }
 
 - (void)initAttributes
 {
-    _annotations = [NSMutableArray array];
     _pois = nil;
-    
-    _longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
-    _longPressGesture.delegate = self;
-    [_mapView addGestureRecognizer:_longPressGesture];
 }
 
 - (void)initTableView
 {
-    CGFloat halfHeight = CGRectGetHeight(self.view.bounds) * 0.5;
     
-    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, halfHeight, CGRectGetWidth(self.view.bounds), halfHeight) style:UITableViewStylePlain];
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds)) style:UITableViewStylePlain];
     _tableView.delegate = self;
     _tableView.dataSource = self;
     
@@ -131,101 +112,6 @@
 }
 
 #pragma mark - Helpers
-
-- (CGSize)offsetToContainRect:(CGRect)innerRect inRect:(CGRect)outerRect
-{
-    CGFloat nudgeRight = fmaxf(0, CGRectGetMinX(outerRect) - (CGRectGetMinX(innerRect)));
-    CGFloat nudgeLeft = fminf(0, CGRectGetMaxX(outerRect) - (CGRectGetMaxX(innerRect)));
-    CGFloat nudgeTop = fmaxf(0, CGRectGetMinY(outerRect) - (CGRectGetMinY(innerRect)));
-    CGFloat nudgeBottom = fminf(0, CGRectGetMaxY(outerRect) - (CGRectGetMaxY(innerRect)));
-    return CGSizeMake(nudgeLeft ?: nudgeRight, nudgeTop ?: nudgeBottom);
-}
-
-- (CLLocationCoordinate2D *)coordinatesForString:(NSString *)string
-                                 coordinateCount:(NSUInteger *)coordinateCount
-                                      parseToken:(NSString *)token
-{
-    if (string == nil)
-    {
-        return NULL;
-    }
-    
-    if (token == nil)
-    {
-        token = @",";
-    }
-    
-    NSString *str = @"";
-    if (![token isEqualToString:@","])
-    {
-        str = [string stringByReplacingOccurrencesOfString:token withString:@","];
-    }
-    
-    else
-    {
-        str = [NSString stringWithString:string];
-    }
-    
-    NSArray *components = [str componentsSeparatedByString:@","];
-    NSUInteger count = [components count] / 2;
-    if (coordinateCount != NULL)
-    {
-        *coordinateCount = count;
-    }
-    CLLocationCoordinate2D *coordinates = (CLLocationCoordinate2D*)malloc(count * sizeof(CLLocationCoordinate2D));
-    
-    for (int i = 0; i < count; i++)
-    {
-        coordinates[i].longitude = [[components objectAtIndex:2 * i]     doubleValue];
-        coordinates[i].latitude  = [[components objectAtIndex:2 * i + 1] doubleValue];
-    }
-    
-    return coordinates;
-}
-
-- (NSArray *)polylinesForPath:(AMapPath *)path
-{
-    if (path == nil || path.steps.count == 0)
-    {
-        return nil;
-    }
-    
-    NSMutableArray *polylines = [NSMutableArray array];
-    
-    [path.steps enumerateObjectsUsingBlock:^(AMapStep *step, NSUInteger idx, BOOL *stop) {
-        
-        NSUInteger count = 0;
-        CLLocationCoordinate2D *coordinates = [self coordinatesForString:step.polyline
-                                                         coordinateCount:&count
-                                                              parseToken:@";"];
-        
-        MAPolyline *polyline = [MAPolyline polylineWithCoordinates:coordinates count:count];
-        [polylines addObject:polyline];
-        
-        free(coordinates), coordinates = NULL;
-    }];
-    
-    return polylines;
-}
-
-- (void)pathAction
-{
-    if (_destinationPoint == nil || _currentLocation == nil || _search == nil)
-    {
-        NSLog(@"path search failed");
-        return;
-    }
-    
-    AMapNavigationSearchRequest *request = [[AMapNavigationSearchRequest alloc] init];
-
-    // 设置为步行路径规划
-    request.searchType = AMapSearchType_NaviWalking;
-    
-    request.origin = [AMapGeoPoint locationWithLatitude:_currentLocation.coordinate.latitude longitude:_currentLocation.coordinate.longitude];
-    request.destination = [AMapGeoPoint locationWithLatitude:_destinationPoint.coordinate.latitude longitude:_destinationPoint.coordinate.longitude];
-    
-    [_search AMapNavigationSearch:request];
-}
 
 - (void)searchAction
 {
@@ -242,15 +128,8 @@
     request.keywords = @"餐饮";
     
     [_search AMapPlaceSearch:request];
-}
-
-- (void)locateAction
-{
-    if (_mapView.userTrackingMode != MAUserTrackingModeFollow)
-    {
-        _mapView.userTrackingMode = MAUserTrackingModeFollow;
-        [_mapView setZoomLevel:kDefaultLocationZoomLevel animated:YES];
-    }
+    
+    [self reGeoAction];
 }
 
 - (void)reGeoAction
@@ -263,33 +142,6 @@
         
         [_search AMapReGoecodeSearch:request];
     }
-}
-
-- (void)handleLongPress:(UILongPressGestureRecognizer *)gesture
-{
-    if (gesture.state == UIGestureRecognizerStateBegan)
-    {
-        CLLocationCoordinate2D coordinate = [_mapView convertPoint:[gesture locationInView:_mapView]
-                                                  toCoordinateFromView:_mapView];
-        
-        // 添加标注
-        if (_destinationPoint != nil)
-        {
-            // 清理
-            [_mapView removeAnnotation:_destinationPoint];
-            _destinationPoint = nil;
-            
-            [_mapView removeOverlays:_pathPolylines];
-            _pathPolylines = nil;
-        }
-        
-        _destinationPoint = [[MAPointAnnotation alloc] init];
-        _destinationPoint.coordinate = coordinate;
-        _destinationPoint.title = @"Destination";
-        
-        [_mapView addAnnotation:_destinationPoint];
-    }
-
 }
 
 #pragma mark - AMapSearchDelegate
@@ -311,111 +163,22 @@
     }
     
     // 更新我的位置title
-    _mapView.userLocation.title = title;
-    _mapView.userLocation.subtitle = response.regeocode.formattedAddress;
+    _titleLabel.text = title;
+    _subtitleLabel.text = response.regeocode.formattedAddress;
 }
 
 - (void)onPlaceSearchDone:(AMapPlaceSearchRequest *)request response:(AMapPlaceSearchResponse *)response
 {
-//    NSLog(@"request: %@", request);
-//    NSLog(@"response: %@", response);
     
     if (response.pois.count > 0)
     {
         _pois = response.pois;
         
         [_tableView reloadData];
-        
-        // 清空标注
-        [_mapView removeAnnotations:_annotations];
-        [_annotations removeAllObjects];
-    }
-}
-
-- (void)onNavigationSearchDone:(AMapNavigationSearchRequest *)request response:(AMapNavigationSearchResponse *)response
-{
-//    NSLog(@"request: %@", request);
-//    NSLog(@"response: %@", response);
-    if (response.count > 0)
-    {
-        [_mapView removeOverlays:_pathPolylines];
-        _pathPolylines = nil;
-        
-        // 只显示第一条
-        _pathPolylines = [self polylinesForPath:response.route.paths[0]];
-        [_mapView addOverlays:_pathPolylines];
-        
-        [_mapView showAnnotations:@[_destinationPoint, _mapView.userLocation] animated:YES];
     }
 }
 
 #pragma mark - MAMapViewDelegate
-
-- (MAOverlayView *)mapView:(MAMapView *)mapView viewForOverlay:(id<MAOverlay>)overlay
-{
-    if ([overlay isKindOfClass:[MAPolyline class]])
-    {
-        MAPolylineView *polylineView = [[MAPolylineView alloc] initWithPolyline:overlay];
-        
-        polylineView.lineWidth   = 4;
-        polylineView.strokeColor = [UIColor magentaColor];
-        
-        return polylineView;
-    }
-    
-    return nil;
-}
-
-- (MAAnnotationView *)mapView:(MAMapView *)mapView viewForAnnotation:(id<MAAnnotation>)annotation
-{
-    if (annotation == _destinationPoint)
-    {
-        static NSString *reuseIndetifier = @"startAnnotationReuseIndetifier";
-        MAPinAnnotationView *annotationView = (MAPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:reuseIndetifier];
-        if (annotationView == nil)
-        {
-            annotationView = [[MAPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:reuseIndetifier];
-        }
-
-        annotationView.canShowCallout = YES;
-        annotationView.animatesDrop = YES;
-
-        return annotationView;
-    }
-    
-    if ([annotation isKindOfClass:[MAPointAnnotation class]])
-    {
-        static NSString *reuseIndetifier = @"annotationReuseIndetifier";
-        CustomAnnotationView *annotationView = (CustomAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:reuseIndetifier];
-        if (annotationView == nil)
-        {
-            annotationView = [[CustomAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:reuseIndetifier];
-        }
-        annotationView.image = [UIImage imageNamed:@"restaurant"];
-        
-        // 设置为NO，用以调用自定义的calloutView
-        annotationView.canShowCallout = NO;
-        
-        // 设置中心点偏移，使得标注底部中间点成为经纬度对应点
-        annotationView.centerOffset = CGPointMake(0, -18);
-        return annotationView;
-    }
-    
-    return nil;
-}
-
-- (void)mapView:(MAMapView *)mapView didChangeUserTrackingMode:(MAUserTrackingMode)mode animated:(BOOL)animated
-{
-    // 修改定位按钮状态
-    if (mode == MAUserTrackingModeNone)
-    {
-        [_locationButton setImage:[UIImage imageNamed:@"location_no"] forState:UIControlStateNormal];
-    }
-    else
-    {
-        [_locationButton setImage:[UIImage imageNamed:@"location_yes"] forState:UIControlStateNormal];
-    }
-}
 
 - (void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation updatingLocation:(BOOL)updatingLocation
 {
@@ -423,36 +186,6 @@
     if (updatingLocation)
     {
         _currentLocation = [userLocation.location copy];
-    }
-}
-
-- (void)mapView:(MAMapView *)mapView didSelectAnnotationView:(MAAnnotationView *)view
-{
-    // 选中定位annotation的时候进行逆地理编码查询
-    if ([view.annotation isKindOfClass:[MAUserLocation class]])
-    {
-        [self reGeoAction];
-    }
-    
-    // 调整自定义callout的位置，使其可以完全显示
-    if ([view isKindOfClass:[CustomAnnotationView class]]) {
-        CustomAnnotationView *cusView = (CustomAnnotationView *)view;
-        CGRect frame = [cusView convertRect:cusView.calloutView.frame toView:_mapView];
-        
-        frame = UIEdgeInsetsInsetRect(frame, UIEdgeInsetsMake(kDefaultCalloutViewMargin, kDefaultCalloutViewMargin, kDefaultCalloutViewMargin, kDefaultCalloutViewMargin));
-        
-        if (!CGRectContainsRect(_mapView.frame, frame))
-        {
-            CGSize offset = [self offsetToContainRect:frame inRect:_mapView.frame];
-            
-            CGPoint theCenter = _mapView.center;
-            theCenter = CGPointMake(theCenter.x - offset.width, theCenter.y - offset.height);
-            
-            CLLocationCoordinate2D coordinate = [_mapView convertPoint:theCenter toCoordinateFromView:_mapView];
-            
-            [_mapView setCenterCoordinate:coordinate animated:YES];
-        }
-        
     }
 }
 
@@ -492,14 +225,7 @@
     // 为点击的poi点添加标注
     AMapPOI *poi = _pois[indexPath.row];
     
-    MAPointAnnotation *annotation = [[MAPointAnnotation alloc] init];
-    annotation.coordinate = CLLocationCoordinate2DMake(poi.location.latitude, poi.location.longitude);
-    annotation.title = poi.name;
-    annotation.subtitle = poi.address;
-    
-    [_mapView addAnnotation:annotation];
-    
-    [_annotations addObject:annotation];
+    NSLog(@"poi :%@", poi);
 }
 
 @end
